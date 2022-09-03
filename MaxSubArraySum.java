d374f981de12381603a82306c4cf132fedada9a4
Riaz Quadri <riazquadri@MacBook-Pro.local>
2022-09-04 00:51:32 +0530

    Maximum Sum Subarray of Size K

---
+++ MaxSubArraySum.java
@@ -0,0 +1,15 @@
+public class MaxSubArraySum {
+    /*
+     * Given an array of positive numbers and a positive number ‘k’, 
+     * find the maximum sum of any contiguous subarray of size ‘k’.
+     * Input : [2, 1, 5, 1, 3, 2]  k = 3
+     * Output : 9
+     */
+    public static int findMaximumSubArraySumOfK(int []nums){
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ MyApp.java
@@ -0,0 +1,9 @@
+/**
+ * MyApp
+ */
+public class MyApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
