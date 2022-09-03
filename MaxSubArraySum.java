cb5b135b49fa4f5dbb27180cba15c8a75d2cb635
Riaz Quadri <riazquadri@MacBook-Pro.local>
2022-09-04 00:56:23 +0530

    Maximum Sum Subarray of Size K

--- MaxSubArraySum.java
+++ MaxSubArraySum.java
@@ -5,11 +5,46 @@ public class MaxSubArraySum {
      * Input : [2, 1, 5, 1, 3, 2]  k = 3
      * Output : 9
      */
-    public static int findMaximumSubArraySumOfK(int []nums){
+    
 
-        return -1;
+     //Time: O(n^2)   Space : O(1)
+    public static int findMaximumSubArraySumOfK1(int []nums, int k){
+        if(nums.length <= 0 || k <= 0){
+            return -1;
+        }
+        int maxSum = Integer.MIN_VALUE;
+        for(int i = 0; i <= nums.length - k; i++){
+            int sum = 0;
+            for(int j = i; j < i + k; j++){
+                sum += nums[j];
+            }
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+    public static int findMaximumSubArraySumOfK2(int [] nums, int k){
+        if(nums.length <= 0 || k <= 0){
+            return -1;
+        }
+        int n = nums.length;
+        int windStart = 0;
+        int maxSubArraySum = Integer.MIN_VALUE;
+        int windowSum = 0;
+        for(int windowEnd = 0; windowEnd < n; windowEnd++){
+            windowSum += nums[windowEnd];
+            if(windowEnd >= k -1){
+                if(windowSum > maxSubArraySum){
+                    maxSubArraySum = windowSum;
+                }
+                windowSum -= nums[windStart];
+                windStart++;
+            }
+        }
+        return maxSubArraySum;
     }
     public static void main(String[] args) {
-        
+        System.out.println(findMaximumSubArraySumOfK2(new int[]{2, 3, 4, 1, 5}, 2));
     }
 }
